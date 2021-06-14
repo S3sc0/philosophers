68e68fa1694a0d41f5a95a91d5731683459486c1
Ayoub Amzouar <aamzouar@e1r12p5.1337.ma>
2021-06-14 14:18:41 +0100

    creating threads but no mutex

--- philo.c
+++ philo.c
@@ -26,29 +26,70 @@ int		valid_number(char *str)
 	return (0);
 }
 
-void	check_arg_errors(int ac, char *av[])
+int		check_arg_errors(int ac, char *av[])
 {
 	int		i;
 
 	if (ac < 5 || ac > 6)
-	{
-		printf("Error: wrong number of arguments\n");
-		exit(1);
-	}
+		return (printf("Error: wrong number of arguments\n") * 0 + 1);
 	i = 1;
 	while (i < ac)
 	{
 		if (valid_number(av[i]))
-		{
-			printf("Error: argument number [%d] isn't a valid number\n", i);
-			exit(1);
-		}
+			return (printf("Error: argument [%d] isn't valid\n", i) * 0 + 1);
 		i++;
 	}
+	return (0);
+}
+
+void	parse_the_data(int ac, char *av[])
+{
+	g_philo_attr.nb_of_philo = ft_atoi(av[1]);
+	g_philo_attr.time_to_die = ft_atoi(av[2]);
+	g_philo_attr.time_to_eat = ft_atoi(av[3]);
+	g_philo_attr.time_to_sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		g_philo_attr.times_to_eat = ft_atoi(av[5]);
+}
+
+void	*philosopher(void *arg)
+{
+	printf("i'm philosopher number [%d]\n", *(int *)arg);
+	free(arg);
+	return NULL;
+}
+
+int		create_philosophers(void)
+{
+	int			i;
+	pthread_t	th[g_philo_attr.nb_of_philo];
+	int			*nb;
+
+	i = 0;
+	while (i < g_philo_attr.nb_of_philo)
+	{
+		nb = malloc(sizeof(int));
+		*nb = i;
+		if (pthread_create(&th[i], NULL, &philosopher, nb))
+			return (printf("Error: can't create the thread [%d]\n", i) * 0 + 1);
+		i++;
+	}
+	i = 0;
+	while (i < g_philo_attr.nb_of_philo)
+	{
+		if (pthread_join(th[i], NULL))
+			return (printf("Error: can't join the thread [%d]\n", i) * 0 + 1);
+		i++;
+	}
+	return (0);
 }
 
 int     main(int ac, char *av[])
 {
-	check_arg_errors(ac, av);
+	if (check_arg_errors(ac, av))
+		return (1);
+	parse_the_data(ac, av);
+	if (create_philosophers())
+		return (1);
 	return (0);
 }
