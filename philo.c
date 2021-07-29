7526693e65e6947831520b58d58a4d78ad0d8090
Ayoub Amzouar <aamzouar@e1r11p6.1337.ma>
2021-07-29 15:08:40 +0100

    start parsing the data

--- philo.c
+++ philo.c
@@ -1,6 +1,41 @@
 #include "philo.h"
 
+int		ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int nb;
+	int large;
+
+	large = 0;
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9' && ++large)
+		nb = nb * 10 + str[i++] - 48;
+	if (large > 10 && sign == -1)
+		return (1);
+	if (large > 10 && sign == 1)
+		return (-1);
+	return (nb * sign);
+}
+
+int		parse_info(int ac, char *av[])
+{
+	
+}
+
 int		main(int ac, char *av[])
 {
+	if (parse_info(ac, av) == 1)
+		return (1);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -7,4 +7,26 @@
 #include <pthread.h>
 #include <time.h>
 
+typedef struct	s_info
+{
+	int		nb_of_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		times_to_eat;
+}				t_info;
+
+typedef struct	s_state
+{
+	t_info			*data;
+	int				id;
+	int				eating;
+	int				sleeping;
+	int				last_time_eat;
+	int				current_eat_time;
+	int				times_eat;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	*output;
+}				t_state;
+
 #endif
